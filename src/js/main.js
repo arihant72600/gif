1dce955d433e04751dcc212038eb76cb939d0cb0
arihant72600 <38707885+arihant72600@users.noreply.github.com>
2018-09-16 07:43:06 -0400

    added environment variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
-function generateGifs(info, tab) {console.log("test");console.log(info.selectionText);}
+function generateGifs(info, tab) {console.log("test");console.log(info.selectionText);console.log(process.env);}
 
 var id = chrome.contextMenus.create({"title":"Generate gifs", "contexts":["selection"], "onclick":generateGifs});
 class App extends React.Component {
