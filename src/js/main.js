402b12698455e0528fee78154bd54c39da8884c9
arihant72600 <38707885+arihant72600@users.noreply.github.com>
2018-09-16 08:59:14 -0400

    added Azure natural language processing to identify key phrases

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
-function generateGifs(info, tab) {console.log("test");console.log(info.selectionText);console.log(process.env);}
+var API_KEY = process.env.API_KEY;
+API_KEY = "77c213a53ef44cab9b0a5bd42ed3faeb";
+function generateGifs(info, tab) {
+  var x =  fetch('https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases', {
+  method: 'POST',
+  headers: {
+    'Ocp-Apim-Subscription-Key': '77c213a53ef44cab9b0a5bd42ed3faeb',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    "documents": [
+      {
+        "id": "1",
+        "text": info.selectionText
+      }
+    ]
+  })
+}).then(function(value){  value.json().then(function(data) {
+    console.log(data.documents[0].keyPhrases);
+
+})});
+}
 
 var id = chrome.contextMenus.create({"title":"Generate gifs", "contexts":["selection"], "onclick":generateGifs});
 class App extends React.Component {
