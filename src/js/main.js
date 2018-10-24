e47fe89a0e81cd53cc66391eea6f173e43d2fde0
Arihant Jain <arihant72600@gmail.com>
2018-10-24 14:33:43 -0700

    updated gitignore

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,51 @@
-import React from 'react';
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
 
-import ReactDOM from 'react-dom';
-var API_KEY = process.env.API_KEY;
-API_KEY = "77c213a53ef44cab9b0a5bd42ed3faeb";
-function generateGifs(info, tab) {
-  var x =  fetch('https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases', {
-  method: 'POST',
-  headers: {
-    'Ocp-Apim-Subscription-Key': '77c213a53ef44cab9b0a5bd42ed3faeb',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    "documents": [
-      {
-        "id": "1",
-        "text": info.selectionText
+        console.log(request);
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.greeting == "hello")
+        sendResponse({farewell: "goodbye"});
+    });
+
+
+    import React from 'react';
+
+    import ReactDOM from 'react-dom';
+    
+    
+    
+    class App extends React.Component {
+    
+      render() {
+    
+        return (
+    
+          <div> Your App injected to DOM correctly! </div>
+    
+        )
+    
       }
-    ]
-  })
-}).then(function(value){  value.json().then(function(data) {
-    console.log(data.documents[0].keyPhrases);
-
-})});
-}
-
-var id = chrome.contextMenus.create({"title":"Generate gifs", "contexts":["selection"], "onclick":generateGifs});
-class App extends React.Component {
-
-    render() {
-  
-      return (
-  
-        <div> Your App injected to DOM correctly! </div>
-  
-      )
-  
+    
     }
-  
-  }
-
-
-
-
-
+    
+    
+    
+    // Message Listener function
+    
+    
+    
+    
+    
+    function injectApp() {
+    
+      const newDiv = document.createElement("div");
+    
+      newDiv.setAttribute("id", "chromeExtensionReactApp");
+    
+      document.body.appendChild(newDiv);
+    
+      ReactDOM.render(<App />, newDiv);
+    
+    }
