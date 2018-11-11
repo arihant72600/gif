4c61e6498c78b3b4cf43cc2abe32ecbb9358eb24
Arihant Jain <arihant72600@gmail.com>
2018-11-10 17:34:17 -0800

    fixed formatting

--- src/js/background.js
+++ src/js/background.js
@@ -1,33 +1,35 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-      console.log(response.farewell);
-    });
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
+    console.log(response.farewell);
   });
+});
 
 API_KEY = "77c213a53ef44cab9b0a5bd42ed3faeb";
 function generateGifs(info, tab) {
-  var x =  fetch('https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases', {
-  method: 'POST',
-  headers: {
-    'Ocp-Apim-Subscription-Key': '77c213a53ef44cab9b0a5bd42ed3faeb',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    "documents": [
-      {
-        "id": "1",
-        "text": info.selectionText
-      }
-    ]
-  })
-}).then(function(value){  value.json().then(function(data) {
-    console.log(data.documents[0].keyPhrases); console.log(tab);
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+  var x = fetch('https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases', {
+    method: 'POST',
+    headers: {
+      'Ocp-Apim-Subscription-Key': '77c213a53ef44cab9b0a5bd42ed3faeb',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "documents": [
+        {
+          "id": "1",
+          "text": info.selectionText
+        }
+      ]
+    })
+  }).then(function (value) {
+    value.json().then(function (data) {
+      console.log(data.documents[0].keyPhrases); console.log(tab);
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
           console.log(response.farewell);
         });
       });
-})});
+    })
+  });
 }
 
-var id = chrome.contextMenus.create({"title":"Generate gifs", "contexts":["selection"], "onclick":generateGifs});
+var id = chrome.contextMenus.create({ "title": "Generate gifs", "contexts": ["selection"], "onclick": generateGifs });
--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,51 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+  function (request, sender, sendResponse) {
 
-        console.log(request);
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting == "hello")
-        sendResponse({farewell: "goodbye"});
-    });
+    console.log(request);
+    console.log(sender.tab ?
+      "from a content script:" + sender.tab.url :
+      "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({ farewell: "goodbye" });
+  });
 
 
-    import React from 'react';
+import React from 'react';
 
-    import ReactDOM from 'react-dom';
-    
-    
-    
-    class App extends React.Component {
-    
-      render() {
-    
-        return (
-    
-          <div> Your App injected to DOM correctly! </div>
-    
-        )
-    
-      }
-    
-    }
-    
-    
-    
-    // Message Listener function
-    
-    
-    
-    
-    
-    function injectApp() {
-    
-      const newDiv = document.createElement("div");
-    
-      newDiv.setAttribute("id", "chromeExtensionReactApp");
-    
-      document.body.appendChild(newDiv);
-    
-      ReactDOM.render(<App />, newDiv);
-    
-    }
+import ReactDOM from 'react-dom';
+
+
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+
+      <div> Your App injected to DOM correctly! </div>
+
+    )
+
+  }
+
+}
+
+
+
+// Message Listener function
+
+
+
+
+
+function injectApp() {
+
+  const newDiv = document.createElement("div");
+
+  newDiv.setAttribute("id", "chromeExtensionReactApp");
+
+  document.body.appendChild(newDiv);
+
+  ReactDOM.render(<App />, newDiv);
+
+}
